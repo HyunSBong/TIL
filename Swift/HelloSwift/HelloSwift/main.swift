7173d48f7eb3da076615d5ac8a0f3c0009caeaa0
HyunSBong <69189272+HyunSBong@users.noreply.github.com>
2020-11-03 23:56:50 +0900

    Xcode를 통한 Swift12

---
+++ Swift/HelloSwift/HelloSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HelloSwift
+//
+//  Created by Hyunsu Bong on 2020/05/10.
+//  Copyright © 2020 Hyunsu Bong. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
