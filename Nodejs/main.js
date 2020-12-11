f681d2739d40624330054070c6718c0ec5a583aa
HyunSBong <sanseng@mju.ac.kr>
2020-12-11 21:03:58 +0900

    글생성 링크 만들기

--- Nodejs/main.js
+++ Nodejs/main.js
@@ -1,7 +1,34 @@
 var http = require('http');
 var fs = require('fs'); 
 var url = require('url');
- 
+
+function templateHTML(title, list, body) {
+  return `
+  <!doctype html>
+   <html>
+   <head>
+     <title>WEB1 - ${title}</title>
+     <meta charset="utf-8">
+   </head>
+   <body>
+     <h1><a href="/">WEB</a></h1>
+     ${list}
+     <a href="/create">create</a>
+     ${body}
+   </body>
+   </html>
+   `;
+}
+function templateList(filelist) {
+  var list = '<ul>'; // 파일리스트를 불러와서 표시
+  var i = 0;
+  while(i < filelist.length) {
+    list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+    i += 1;
+  }
+  list += '</ul>';
+  return list;
+}
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
@@ -14,82 +41,47 @@ var app = http.createServer(function(request,response){
           console.log(filelist);
           var title = 'Welcome';
           var description = 'Hello, Node.js';
-          /*
-          var list = `<ul>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>`
-        */
-          var list = '<ul>';
-          
-          var i = 0;
-          while(i < filelist.length) {
-            list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-            i += 1;
-          }
-          list += '</ul>'
-
-          var template = `
-         <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
+          var list = templateList(filelist);
+          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
           response.writeHead(200); //파일이 성공적으로 전송됨
           response.end(template);
         })
         
       } else {
         fs.readdir('/Users/hyunsubong/Developer/Web/TIL/data(web)', function(err, filelist) {
-          console.log(filelist);
-          var title = 'Welcome';
-          var description = 'Hello, Node.js';
-          var list = '<ul>'; // 파일리스트를 불러와서 표시
-          
-          var i = 0;
-          while(i < filelist.length) {
-            list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-            i += 1;
-          }
-          list += '</ul>';
-
           fs.readFile(`/Users/hyunsubong/Developer/Web/TIL/data(web)/${queryData.id}`, 'utf8', function(err, description){
             var title = queryData.id;
-            var template = `
-            <!doctype html>
-            <html>
-            <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              <ul>
-                <li><a href="/?id=HTML">HTML</a></li>
-                <li><a href="/?id=CSS">CSS</a></li>
-                <li><a href="/?id=JavaScript">JavaScript</a></li>
-              </ul>
-              <h2>${title}</h2>
-              <p>${description}</p>
-            </body>
-            </html>
-            `;
+            var list = templateList(filelist);
+            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
             response.writeHead(200); //파일이 성공적으로 전송됨
             response.end(template);
           });
         });
       }
-    } else {
+    } else if (pathname == '/create') {
+      fs.readdir('/Users/hyunsubong/Developer/Web/TIL/data(web)', function(err, filelist) {
+        console.log(filelist);
+        var title = 'WEB - create';
+        var description = 'Hello, Node.js';
+        var list = templateList(filelist);
+        var template = templateHTML(title, list, `
+        <form action="http://localhost:3000/process_create"
+        method="post"> 
+          <p><input type="text" name="title" placehoder="제목에 아무거나 입력해보세요"></p>
+          <p>
+            <textarea name="description" placehoder="본문에 아무거나 입력해보세요"></textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+
+        `);
+        response.writeHead(200); //파일이 성공적으로 전송됨
+        response.end(template);
+      })
+    }
+      else {
       response.writeHead(404); // 에러발생
       response.end('Not found');
     }
