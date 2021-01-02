430221d2c42aa78a0041e841d0e4fb3f9768e95c
HyunSBong <sanseng@mju.ac.kr>
2021-01-02 21:19:38 +0900

    api

---
+++ SpringBoot/src/main/java/com/hsbong/project/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.hsbong.project.springboot.web.dto;
+
+import com.hsbong.project.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
