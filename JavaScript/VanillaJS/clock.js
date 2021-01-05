a8d9b3e0159019150bbd2a2c28d85ffeeda15400
HyunSBong <sanseng@mju.ac.kr>
2021-01-05 18:29:21 +0900

    add day/week

--- JavaScript/VanillaJS/clock.js
+++ JavaScript/VanillaJS/clock.js
@@ -1,8 +1,15 @@
 const clockContainer = document.querySelector(".js-clock");
 const clockTitle = clockContainer.querySelector("h1");
+const dayContainer = document.querySelector(".js-day");
+const clockDay = dayContainer.querySelector("h5");
 
 function getTime() {
     const date = new Date();
+    const year = date.getFullYear();
+    const month = (date.getMonth()+1);
+    const day1 = date.getDate();
+    const weeks = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const day2 = date.getDay();
     const minutes = date.getMinutes();
     const hours = date.getHours();
     const seconds = date.getSeconds();
@@ -10,11 +17,14 @@ function getTime() {
         minutes < 10 ? `0${minutes}` : minutes}:${
         seconds < 10 ? `0${seconds}` : seconds
     }`;
+    clockDay.innerText = `${year} . ${month < 10 ? `0${month}`: month} . ${
+        day1 < 10 ? `0${day1}`: day1} . ${weeks[day2]}`;
 }
+
 
 function init() {
     getTime();
     setInterval(getTime, 1000);
 }
 
-init();;
+init();
