61346b2902c502e6e4d99ba19260d77dc0f73ec7
HyunSBong <sanseng@mju.ac.kr>
2021-01-08 15:34:14 +0900

    Creating first React Component

--- React/my_movie_app_2021/src/App.js
+++ React/my_movie_app_2021/src/App.js
@@ -1,9 +1,11 @@
-// import logo from './logo.svg';
-// import './App.css';
+import React from 'react';
+import Potato from './Potato';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <h1>Hello!</h1>
+      <Potato />
     </div>
   );
 }
--- React/my_movie_app_2021/src/index.js
+++ React/my_movie_app_2021/src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// <App /> 는 Component이며 HTML을 반환하는 함수
+// React는 componet를 사용하여 HTML처럼 작성하려하는데 이를 JSX라고 함
+// React Application은 한 번에 하나의 component만을 rendering할 수 있음
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
