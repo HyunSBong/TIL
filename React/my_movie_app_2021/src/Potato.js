ece682d3e8fad4568bed4093d3aefb63ab20bb18
HyunSBong <sanseng@mju.ac.kr>
2021-01-08 15:46:28 +0900

    delete

--- React/my_movie_app_2021/src/Potato.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-// Component를 작성할 때 마다 React를 import해야 jsx가 있는 component를 이해할 수 있음.(매우중요!)
-
-function Potato() {
-    return <h3>I love potato</h3>
-}
-
-export default Potato;
