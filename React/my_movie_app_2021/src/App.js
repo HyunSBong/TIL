02a9115c4d87b79c02e7f4ae871144995a6ce192
HyunSBong <sanseng@mju.ac.kr>
2021-01-10 13:42:19 +0900

    setTimeout

--- React/my_movie_app_2021/src/App.js
+++ React/my_movie_app_2021/src/App.js
@@ -2,39 +2,18 @@ import React from "react";
 
 class App extends React.Component {
   state = {
-    count: 0
+    isLoading: true
   };
-  add = () => {
-    this.setState(current => ({ count: current.count + 1 }));
-    // current = this.state
-    // react는 setState를 호출할 때마다 새로운 state와 함께 render function을 호출함
-  };
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1 }));
-  };
-  // Component Life Cycle
-  /*
-  실행 - Render Mount
-  setState - Render Update
-  페이지 나갈 때 - Will Umnount
-  */
   componentDidMount() {
-    console.log("Component rendered");
+    setTimeout(() => {
+      this.setState({isLoading: false});
+    }, 6000);
   }
-  componentDidUpdate() {
-    console.log("I just updated");
-  }
-  componentWillUnmount() {
-    console.log("Goodbye");
-  }
-
   render() {
     console.log("I'm rendering");
     return (
       <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
+        {this.state.isLoading ? "Loading" : "We are ready"}
       </div>
     );
   }
