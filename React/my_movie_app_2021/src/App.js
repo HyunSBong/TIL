b4a879441dad78c8a51f00eae2ab17c032f6971a
HyunSBong <sanseng@mju.ac.kr>
2021-01-10 14:16:01 +0900

    getData(PropTypes)

--- React/my_movie_app_2021/src/App.js
+++ React/my_movie_app_2021/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./movie";
 
 class App extends React.Component {
   state = {
@@ -7,8 +8,13 @@ class App extends React.Component {
     movies: []
   };
   getMovies = async() => { // async 비동기식으로 변경 -> api가 모두 로드될 때까지 기다리기 위해
-    const movies = axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json ");
-  }
+    const {
+      data: {
+        data: {movies}
+      }
+    } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
+    this.setState({movies, isLoading: false})
+  };
   componentDidMount() {
     this.getMovies();
   }
@@ -16,7 +22,18 @@ class App extends React.Component {
     // const {isLoading} = this.state;
     return (
       <div>
-        {this.state.isLoading ? "Loading" : "We are ready"}
+        {this.state.isLoading ? "Loading" : this.state.movies.map(movie => {
+          console.log(movie);
+          return (
+          <Movie 
+            key={movie.id}
+            id={movie.id} 
+            year={movie.year} 
+            title={movie.title} 
+            summary={movie.summary} 
+            poster={movie.poster} />
+          );
+        })}
       </div>
     );
   }
